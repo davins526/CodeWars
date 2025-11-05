8422df9ce60efd0ef878bd64058296b0a8a42b9f
Davins <danie2dickson3@gmail.com>
2025-11-05 20:52:18 +0500

    Add project files.

---
+++ tse/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeWars;
+
+public static class Task
+{
+    public static string RepeatStr(int n, string s)
+    {
+        string R = string.Empty;
+
+        for (int i = 0; i <= n; i++)
+        {
+            R += s;
+        }
+        return R;
+    } 
+}
