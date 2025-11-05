b26c1522833b10066bb397559209fdc2472d517f
Davins <danie2dickson3@gmail.com>
2025-11-05 21:00:42 +0500

    Calculate average Fundamentals Write a function which calculates the average of the numbers in a given array. time : 3259ms

---
+++ tse/CodeWarsProblems.cs
@@ -0,0 +1,16 @@
+﻿class AverageSolution
+{
+    public static double FindAverage(double[] array)
+    {
+        double sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+            count++;
+        }
+        return sum / count;
+
+    }
+}
