52198bd85177245eae4512cd6e38f70c2a864c3a
Davins <danie2dickson3@gmail.com>
2025-11-06 11:08:17 +0500

    Returning Strings You have passed all of the tests! :) Completed in 19.6440ms

--- tse/CodeWarsProblems.cs
+++ tse/CodeWarsProblems.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 public static class Kata
 {
-    public static int CountSheeps(bool[] sheeps)
+    public static string Greet(string name)
     {
-            return sheeps.Count(s => s);
+        return ($"Hello, {name} how are you doing today?")
     }
 }
